cf1e19985b52c6989339e6c0a8d9d53b6785e712
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-07-03 00:30:17 +0900

    Modified "return statement"

--- News App Next.js/news-app/pages/index.js
+++ News App Next.js/news-app/pages/index.js
@@ -1,15 +1,21 @@
 import Header from "./layout/Header";
 
 const Index = () => {
-  <>
-    <Header />
-    <main
-      style={{ display: "flex", flexDirection: "column", alignItem: "center" }}
-    >
-      <h1>Welcome to News App!!</h1>
-      <p>You can check English News</p>
-    </main>
-  </>;
+  return (
+    <>
+      <Header />
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItem: "center",
+        }}
+      >
+        <h1>Welcome to News App!!</h1>
+        <p>You can check English News</p>
+      </main>
+    </>
+  );
 };
 
 export default Index;
